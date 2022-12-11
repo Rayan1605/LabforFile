80d9b0e27fd32436ecd9fd8f2a6d0b1187119737
rayan <rayanzyad99@gmail.com>
2022-12-11 17:16:44 +0000

    Part2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     //File is an abstract representation of file and directory pathnames
     public static void main(String[] args) {
         File file = new File("message.txt");
-       if (file.exists()){ // just to check if it exist in the same project folder
+
+        if (file.exists()){ // just to check if it exist in the same project folder
            System.out.println(file.getPath());
            System.out.println(file.getAbsoluteFile());
            System.out.println(file.length());
@@ -12,6 +15,15 @@ public class Main {
        else{
            System.out.println("that file does not exist ");
        }
+        try {
+            FileWriter writer = new FileWriter("message.txt",true);
+            writer.write("\n I  am  very close to finishing my  java project ");
+
+            writer.close();
+        } catch (IOException e) {  // Just in case  the  input failed .
+            System.out.println("An error occurred   writing to file ");
+            e.printStackTrace();
+        }
 
     }
 }
